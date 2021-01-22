44d79a7b4f40bdd44eb771b3ea80d609e6abfcc2
GueltonGeorges1995 <49947507+GueltonGeorges1995@users.noreply.github.com>
2021-01-22 17:26:38 +0100

    test2

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 let cors = require('cors')
 const app = express();
-app.use(cors())
+app.use(cors());
+
+
+var corsOptions = {
+    origin: 'https://server-node-first-test.vercel.app/blogs',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 
 
 app.get('/', (req, res) => res.send('Home Page Route'));
-app.get('/blogs', (req, res) => res.send({
+
+
+// app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+//     res.json({msg: 'This is CORS-enabled for only example.com.'})
+//   })
+  
+
+
+app.get('/blogs', cors(corsOptions), (req, res) => res.send({
     "blogs":[
         {
             "title": "My first blog",
@@ -32,6 +47,7 @@ app.get('/blogs', (req, res) => res.send({
 // app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
 const port = process.env.PORT || 80;
+
 
 // app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
 
