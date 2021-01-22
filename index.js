606eaf07b3b3c44d3917e6b04a8b3b570c6be1ef
GueltonGeorges1995 <49947507+GueltonGeorges1995@users.noreply.github.com>
2021-01-22 16:35:51 +0100

    fix data blogs

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Home Page Route'));
 app.get('/blogs', (req, res) => res.send({
-    title: "test1",
-    author: "Georges",
-    id: 1
-}));
+    "blogs":[
+        {
+            "title": "My first blog",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla in aperiam sint dolores doloribus? Consequuntur ullam reiciendis maiores ratione natus aliquid rerum molestiae. Inventore magni velit ipsam aut, voluptas asperiores?",
+            "author": "mario",
+            "id": 1
+        },
+        {
+            "title": "testing party",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla in aperiam sint dolores doloribus? Consequuntur ullam reiciendis maiores ratione natus aliquid rerum molestiae. Inventore magni velit ipsam aut, voluptas asperiores?",
+            "author": "Pierro",
+            "id": 2
+        }
+    ]
+    
+    
+}
+));
 
 // app.get('/about', (req, res) => res.send('About Page Route'));
 
